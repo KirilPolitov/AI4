267f667531f546e4520ce0123296839237f20f41
KirilPolitov <polito.kiril@gmail.com>
2026-01-22 10:46:26 +0200

    Add age input and validation for login check

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 st.title("login")
 name = st.text_input("enter name")
-if st.button("check"):
+age = st.number_input("enter age")
+if st.button("check") and age >= 18:
  if name.strip() == "":
   st.warning("please enter text")
  elif not name.isalpha():
   st.warning("pls text")
  else: st.success("good")
+else: st.warning("wait a few years")
