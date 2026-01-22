1d67791cbccec1986c96b88887abe5f7156a0e4e
KirilPolitov <polito.kiril@gmail.com>
2026-01-22 10:44:10 +0200

    Fix input validation for empty name field

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("login")
 name = st.text_input("enter name")
 if st.button("check"):
- if name.strip() == " ":
+ if name.strip() == "":
   st.warning("please enter text")
  elif not name.isalpha():
   st.warning("pls text")
